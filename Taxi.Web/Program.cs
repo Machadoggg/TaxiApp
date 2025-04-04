32c564c2b8c99cc2001d65a6699fc4194c9bbb28
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-04-04 10:09:42 -0500

    Add project files.

---
+++ Taxi.Web/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Taxi.Web.Data;
+
+namespace Taxi.Web
+{
+    public class Program
+    {
+
+
+
+        public static void Main(string[] args)
+        {
+            // Agrega esto al inicio de tu aplicación (Program.cs)
+            System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+                (sender, certificate, chain, sslPolicyErrors) => true;
+
+
+            IWebHost host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
+        }
+
+        private static void RunSeeding(IWebHost host)
+        {
+            IServiceScopeFactory scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (IServiceScope scope = scopeFactory.CreateScope())
+            {
+                SeedDb seeder = scope.ServiceProvider.GetService<SeedDb>();
+                seeder.SeedAsync().Wait();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+    }
+}
